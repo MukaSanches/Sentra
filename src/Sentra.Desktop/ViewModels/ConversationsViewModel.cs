49e8a0e2f8ef87c25eef9a2e990e2922102c12b0
MukaSanches <ssanches18@icloud.com>
2026-09-20 22:18:29 -0300

    fix(v1): reference intelligence project and contracts

--- src/Sentra.Desktop/ViewModels/ConversationsViewModel.cs
+++ src/Sentra.Desktop/ViewModels/ConversationsViewModel.cs
@@ -2,6 +2,7 @@ using System.Collections.ObjectModel;
 using System.IO;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using Sentra.Contracts.Operations;
 using Sentra.Contracts.WhatsApp;
 using Sentra.Desktop.Services;
 
